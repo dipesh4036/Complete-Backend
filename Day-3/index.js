24558ac010407b53997636a2da76f690a0ef8a6e
Sojitra Dipesh <111883699+dipesh4036@users.noreply.github.com>
2024-11-06 17:42:56 +0530

    Update index.js

--- Day-3/index.js
+++ Day-3/index.js
@@ -13,6 +13,8 @@ app.get('/contant',function (req,res) {
     res.send('contant page haow')
 })
 
+// Define a middleware function that runs on every request
+// Logs "heelo !" to the console, then calls `next()` to continue to the next route or middleware
 app.use(function (req,res,next) {
     console.log("heelo !");
     next();
